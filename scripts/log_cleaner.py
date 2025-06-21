6c179612ca1a5b9df846fd0c33afe6c7d5e8256b
Sass Péter <sass.peter43@gmail.com>
2025-06-21 22:38:16 +0200

    log_cleaner.py

--- scripts/log_cleaner.py
+++ scripts/log_cleaner.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
+import os
+
+print("[*] Attempting to clear bash history and logs...")
+os.system("history -c")
+os.system("rm -f ~/.bash_history")
+print("[*] Done.")
 # Bash history and syslog clearer
